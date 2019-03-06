3be08102b1d7cde3ff55ee4dba3b81f442ec3fef
ahangchen <1418659400@qq.com>
2019-03-06 16:38:45 +0800

    publish to cweihang.io

--- publish.sh
+++ publish.sh
@@ -1,17 +1,10 @@
 gitbook build . ../tmp_mysite
 cd ../mysite
 rm -rf `ls |grep -v .git`
-cd ../ahangchen.github.io
-rm -rf `ls |grep -v .git`
 cp -r ../tmp_mysite/* ../mysite
-cp -r ../tmp_mysite/* ../ahangchen.github.io
 rm -rf ../tmp_mysite
 cd ../mysite
 git add -A
 git commit -a -m 'update from gitbook'
 git push origin master
-cd ../ahangchen.github.io
-git add -A
-git commit -a -m 'update from gitbook'
-git push origin master
-ssh wemeet@cweihang.cn "cd mysite;git pull"
+ssh cwh@cweihang.io "cd mysite;git pull"
