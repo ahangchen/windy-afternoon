3973b4ba366e545b2aa99aa9a41cfa0ef5416da7
ahangchen <1418659400@qq.com>
2021-10-03 11:02:26 +0800

    publish.sh for github.io

--- publish.sh
+++ publish.sh
@@ -1,10 +1,19 @@
 gitbook build . ../tmp_mysite
-cd ../mysite
-rm -rf `ls |grep -v .git`
-cp -r ../tmp_mysite/* ../mysite
+
+function publish_to()
+{
+    repo_name=$1
+    cd ../${repo_name}
+    rm -rf `ls |grep -v .git`
+    cp -r ../tmp_mysite/* ../${repo_name}
+    cd ../${repo_name}
+    git add -A
+    git commit -a -m 'update from gitbook'
+    git push
+}
+
+publish_to mysite
+publish_to ahangchen.github.io
+
 rm -rf ../tmp_mysite
-cd ../mysite
-git add -A
-git commit -a -m 'update from gitbook'
-git push origin master
 # ssh cwh@cweihang.io "cd mysite;git pull"
